9ca96f095d98055ac71b55c09589e8e17ed3f227
Sergei Polusmiak <serpolus@gmail.com>
2021-08-06 11:11:47 -0400

    see all users by family id backend

--- java/src/main/java/com/techelevator/dao/SessionDao.java
+++ java/src/main/java/com/techelevator/dao/SessionDao.java
@@ -1,10 +1,15 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.Session;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 public interface SessionDao {
 
     boolean createSession(String isbn, Long userId, LocalDate dayOfSession, LocalTime startTime, LocalTime endTime, String format, String notes);
+
+    List<Session> findAllSessionsInFamily(Long familyId);
 
 }
