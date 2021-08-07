be9876aa0066e4a39da170924aabf5fbc3cce030
Bridgitte <bwaldrop06@gmail.com>
2021-08-07 13:09:04 -0400

    updates to frontend

--- vue/src/views/Prizes.vue
+++ vue/src/views/Prizes.vue
@@ -1,1 +1,28 @@
 <!--begin form here to add prizes for books read -->
+<template>
+  <div class="prizes">
+    <h2 class="prize-name">Add Prize</h2>
+    <label for="prize-name" class="sr-only"> Prize: </label>
+    <input
+      type="text"
+      id="prize"
+      class="form-control"
+      placeholder="Prize"
+      v-model="prize.name"
+      required
+      autofocus
+    />
+
+    <label for="to-redeem" class="sr-only"> To Redeem: </label>
+    <input
+      type="text"
+      id="to-redeem"
+      class="form-control"
+      placeholder="To Redeem"
+      v-model="prize.cost"
+      required
+      autofocus
+    />
+
+  </div>
+</template>
